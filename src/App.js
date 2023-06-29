c597847b9d305feb13c47b69a0eb882c0f5b10bc
Taraansh <taraanshnandwani123@gmail.com>
2023-06-29 20:40:49 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import NavBar from "./components/NavBar";
+import { AuthProvider } from './context/AuthContext'
+import SignUp from "./components/SignUp";
+import Profile from "./components/Profile";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<BrowserRouter>
+      <AuthProvider>
+    <NavBar/>
+      <Routes>
+
+        <Route path="/Signup" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+</>
   );
 }
 
