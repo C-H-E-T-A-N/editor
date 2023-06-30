8eeece42d6e9ce629b9cad3e561002534207ef5a
Taraansh <taraanshnandwani123@gmail.com>
2023-06-30 23:45:12 +0530

    Started HomePagee

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import PrivateRoute from "./utils/PrivateRoute";
 import NavBar from "./components/NavBar";
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from "./context/AuthContext";
 import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 import WebBuilder from "./components/WebBuilder";
-
+import PasswordChange from "./components/PasswordChange";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-<>
-<BrowserRouter>
-      <AuthProvider>
-    <NavBar/>
-      <Routes>
-
-        <Route path="/Signup" element={<SignUp />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Editor" element={<WebBuilder />} />
-      </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-</>
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+          <NavBar />
+          <Routes>
+            <Route
+              path="/Dashboard"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<SignUp />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/PwdChange" element={<PasswordChange />} />
+            <Route path="/" element={<WebBuilder />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </>
   );
 }
 
