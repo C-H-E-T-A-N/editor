186ed14d4e2ffcb154ec8ee1af71678316d9e56d
Taraansh <taraanshnandwani123@gmail.com>
2023-07-03 15:18:40 +0530

    Passed props

--- src/App.js
+++ src/App.js
@@ -8,8 +8,29 @@ import Profile from "./components/Profile";
 import WebBuilder from "./components/WebBuilder";
 import PasswordChange from "./components/PasswordChange";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import { useState } from "react";
+import NewEditor from "./components/NewEditor";
 
 function App() {
+  // const [id, setId] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [html_content, setHtmlContent] = useState("");
+  // const [css_content, setCssContent] = useState("");
+
+  // const handleValueFromChild = (id, title, html_content, css_content) => {
+  //   setId(id);
+  //   setTitle(title);
+  //   setHtmlContent(html_content);
+  //   setCssContent(css_content);
+  // };
+
+  const [selectedPage, setSelectedPage] = useState("");
+
+  const handleEditPage = (pageData) => {
+    setSelectedPage(pageData);
+  };
+
   return (
     <>
       <BrowserRouter>
@@ -20,7 +41,7 @@ function App() {
               path="/Dashboard"
               element={
                 <PrivateRoute>
-                  <HomePage />
+                  <Dashboard onEditPage={handleEditPage} />
                 </PrivateRoute>
               }
             />
@@ -28,7 +49,17 @@ function App() {
             <Route path="/Signup" element={<SignUp />} />
             <Route path="/Profile" element={<Profile />} />
             <Route path="/PwdChange" element={<PasswordChange />} />
-            <Route path="/" element={<WebBuilder />} />
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/Create"
+              element={
+                selectedPage.id ? (
+                  <WebBuilder {...selectedPage} />
+                ) : (
+                  <NewEditor />
+                )
+              }
+            />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
